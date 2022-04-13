b3e8a2b49dae6dfa01d1129034ebb47d47114c08
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-04-13 17:40:30 +0400

    Add files via upload

---
+++ project_1/firsPP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace firsPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, include digit");
+            int  number = Convert.ToInt32(Console.ReadLine());
+            
+            if(number%2 == 0){
+                Console.WriteLine("This digit is even"); }
+            else
+            {
+                Console.WriteLine("This digit is odd");
+            }
+            
+        }
+    }
+}
